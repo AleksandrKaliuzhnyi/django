05270d54387b0b16daf2073cbc4eb35e3d70a89b
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-08-24 10:05:56 +0300

    Save the task to DB - Django

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -12,8 +12,18 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form was invalid'
+
     form = TaskForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'main/create.html', context)
